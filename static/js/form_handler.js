1a8f10fda9f242c0b4d66780f2bc20df14996c09
Dominik Imrich <dominik.imrich@student.tuke.sk>
2023-03-17 18:41:00 +0100

    add blocker for form field

--- static/js/form_handler.js
+++ static/js/form_handler.js
@@ -1,11 +1,49 @@
-document.getElementById("search_form").addEventListener("submit", myF);
+document.getElementById("search_form").addEventListener("submit", handleFormValues);
+const classroom_element = document.getElementById('classroom_id');
+const equipment_checkboxes = document.getElementById('equipment_id');
 
-function myF(event) {
-    console.log(event);
-    const room = document.getElementById("classroom");
-    const interactive_board = document.getElementById("inter_board");
+classroom_element.addEventListener('input', () => {
+    if (classroom_element.value !== "") {
+        const inputs = equipment_checkboxes.querySelectorAll('input[type="checkbox"]');
+        for (const checkbox of inputs) {
+            if (checkbox.checked) {
+                checkbox.checked = false;
+            }
+        }
+    }
+    equipment_checkboxes.disabled = classroom_element.value !== "";
+});
+equipment_checkboxes.addEventListener('input', () => {
+    const inputs = equipment_checkboxes.querySelectorAll('input[type="checkbox"]');
+    for (const checkbox of inputs) {
+        if (checkbox.checked) {
+            classroom_element.value = "";
+            classroom_element.disabled = true;
+            return;
+        }
+    }
+    classroom_element.disabled = false;
+});
 
-    console.log(interactive_board.innerText);
 
-    //event.preventDefault();
+function handleFormValues(event) {
+    event.preventDefault();
+    const classroomInput = document.getElementById('classroom_id');
+    const formData = new FormData(event.target);
+    //validacia ci je zadana ucebna alebo hodina
+    if (formData.get("classroom") === "" && formData.get("lesson") === "") {
+        classroomInput.setCustomValidity("Nezadaná učebňa ani hodina!")
+        return
+    }
+
+    if (formData.get("classroom") === "") {
+        formData.delete('classroom');
+    }
+    if (formData.get("lesson") === "") {
+        formData.delete('lesson');
+    }
+
+    const params = new URLSearchParams(formData);
+    window.location.href = `/search/?${params.toString()}`;
+
 }
